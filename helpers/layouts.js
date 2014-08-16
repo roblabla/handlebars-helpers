16d7f8ed2a419fddb67f7161b41012a486f9024d
roblabla <robinlambertz.dev@gmail.com>
2014-08-16 02:20:19 +0200

    Allow for disabling cache

--- helpers/layouts.js
+++ helpers/layouts.js
@@ -4,7 +4,7 @@ var _     = require("lodash");
 
 options = _.defaults(options, {include: {
   lookup: function(path) { return path; },
-  cache: {}
+  cache: null
 }});
 if (typeof Handlebars === "undefined")
   Handlebars = require("handlebars");
@@ -21,9 +21,15 @@ return {
     else
       context = this[context];
     name = options.include.lookup(name);
-    if (!(name in options.include.cache))
-      options.include.cache[name] = Handlebars.compile(fs.readFileSync(name, 'utf8'));
-    return new Handlebars.SafeString(options.include.cache[name](context));
+    var template;
+    if (options.include.cache && name in options.include.cache) {
+      template = options.include.cache[name];
+    else {
+      template = Handlebars.compile(fs.readFileSync(name, 'utf8'));
+      if (options.include.cache)
+        options.include.cache[name] = Handlebars.compile(fs.readFileSync(name, 'utf8'));
+    }
+    return new Handlebars.SafeString(template(context));
   }
 };
 };
