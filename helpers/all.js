3952a80da5e57e93f7b4e3d6611b0c1f1d2c8723
roblabla <robinlambertz.dev@gmail.com>
2014-08-17 15:26:06 +0200

    Extend/Block-style templating done. Some restructuring of options system

--- helpers/all.js
+++ helpers/all.js
@@ -7,8 +7,10 @@ var _     = require('lodash');
 var returnValue = {};
 
 fs.readdirSync(path.join(__dirname)).forEach(function(file) {
-  if (file != 'all.js')
-    _.extend(returnValue, require('./'+ file)(Handlebars, options));
+  if (file != 'all.js') {
+    var opts = options[path.basename(file, '.js')];
+    _.extend(returnValue, require('./'+ file)(Handlebars, opts));
+  }
 });
 return returnValue;
 };
--- helpers/layouts.js
+++ helpers/layouts.js
@@ -2,14 +2,28 @@ module.exports = function(Handlebars, options) {
 var fs    = require("fs");
 var _     = require("lodash");
 
-options = _.defaults(options, {include: {
+options = _.defaults(options, {
   lookup: function(path) { return path; },
   cache: null
-}});
+});
 if (typeof Handlebars === "undefined")
   Handlebars = require("handlebars");
+
+function getTemplate(name) {
+  name = options.lookup(name);
+  var template;
+  if (options.cache && name in options.cache)
+    template = options.cache[name];
+  else {
+    template = Handlebars.compile(fs.readFileSync(name, 'utf8'));
+    if (options.cache)
+      options.cache[name] = template;
+  }
+  return template;
+}
+
 return {
-  // {{ include [name] (context) }}
+  // `{{include [name] (context)}}`
   //
   // Expression Helper
   //
@@ -18,18 +32,61 @@ return {
   include: function (name, context) {
     if (typeof context === "undefined")
       context = this;
-    else
-      context = this[context];
-    name = options.include.lookup(name);
-    var template;
-    if (options.include.cache && name in options.include.cache)
-      template = options.include.cache[name];
-    else {
-      template = Handlebars.compile(fs.readFileSync(name, 'utf8'));
-      if (options.include.cache)
-        options.include.cache[name] = Handlebars.compile(fs.readFileSync(name, 'utf8'));
+    return new Handlebars.SafeString(getTemplate(name)(context));
+  },
+
+  // `{{# extend [name]}}`
+  //
+  // Block Helper
+  //
+  // Executes the given template
+  extend: function (name, options) {
+    options.fn(this);
+    return new Handlebars.SafeString(getTemplate(name)(this));
+  },
+
+  // `{{block [name]}}`
+  //
+  // Hybrid Helper
+  //
+  // Expression Form : Places a block at specified position with no content
+  //
+  // Block Form : Places a block at the specified position, with the specified
+  // content
+  block: function(name, options) {
+    var defval = options ? options.fn(this) : "";
+    if (typeof this._blocks === "undefined") this._blocks = {};
+    if (name in this._blocks) {
+      switch (this._blocks[name].type) {
+      case "append":
+        return defval + this._blocks[name].content;
+      case "prepend":
+        return this._blocks[name].content + defval;
+      case "replace":
+        return this._blocks[name].content;
+      }
     }
-    return new Handlebars.SafeString(template(context));
+  },
+
+  //! TODO : Fix nested extends !
+  // `{{#replace [name]}}`
+  //
+  // Block Helper : 
+  replace: function(name, options) {
+    if (typeof this._blocks === "undefined") this._blocks = {};
+    this._blocks[name] = { type: "replace", content: options.fn(this) };
+  },
+
+  // `{{#append [name]}}`
+  append: function(name, options) {
+    if (typeof this._blocks === "undefined") this._blocks = {};
+    this._blocks[name] = { type: "append", content: options.fn(this) };
+  },
+
+  // `{{#prepend [name]}}`
+  prepend: function(name, options) {
+    if (typeof this._blocks === "undefined") this._blocks = {};
+    this._blocks[name] = { type: "prepend", content: options.fn(this) };
   }
 };
 };
